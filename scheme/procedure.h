0e0fa51fc2be447355f8b3ddd6fe124421af49ca
ltoddy <taoliu14@acm.org>
2018-05-15 07:27:57 +0800

    the primitive procedure

---
+++ scheme/procedure.h
@@ -0,0 +1,24 @@
+#ifndef SCHEME_C_PROCEDURE_H
+#define SCHEME_C_PROCEDURE_H
+
+#include "object.h"
+
+extern SchemeObject* MakePrimitiveProc(SchemeObject* (* fn)(SchemeObject* arguments));
+
+extern char IsPrimitiveProc(SchemeObject* obj);
+
+extern SchemeObject* AddProcedure(SchemeObject* arguments);
+
+extern char IsApplication(SchemeObject* exp);
+
+extern SchemeObject* Operator(SchemeObject* exp);
+
+extern SchemeObject* Operands(SchemeObject* exp);
+
+extern char IsNoOperands(SchemeObject* ops);
+
+extern SchemeObject* FirstOperand(SchemeObject* exp);
+
+extern SchemeObject* RestOperands(SchemeObject* exp);
+
+#endif //SCHEME_C_PROCEDURE_H
