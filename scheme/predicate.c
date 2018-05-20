28412a857dca37bfbc516c6ba519a487ee83d1e8
ltoddy <taoliu14@acm.org>
2018-05-20 08:52:06 +0800

    add 'and' and 'or' procedure.

--- scheme/predicate.c
+++ scheme/predicate.c
@@ -1,6 +1,26 @@
 #include "predicate.h"
 #include "assignment.h"
 
+char IsAnd(SchemeObject* exp)
+{
+    return IsTaggedList(exp, AndSymbol);
+}
+
+SchemeObject* AndTests(SchemeObject* exp)
+{
+    return Cdr(exp);
+}
+
+char IsOr(SchemeObject* exp)
+{
+    return IsTaggedList(exp, OrSymbol);
+}
+
+SchemeObject* OrTests(SchemeObject* exp)
+{
+    return Cdr(exp);
+}
+
 char IsTaggedList(SchemeObject* exp, SchemeObject* tag)
 {
     SchemeObject* car;
